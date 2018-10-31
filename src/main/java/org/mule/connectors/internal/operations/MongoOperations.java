3840c8f95bc1de752372bff9cf9f5f2e858d3003
estebanwasinger <estebanwasinger@gmail.com>
2018-10-30 22:57:30 -0300

    Add insert document operation

--- src/main/java/org/mule/connectors/internal/operations/MongoOperations.java
+++ src/main/java/org/mule/connectors/internal/operations/MongoOperations.java
@@ -1,25 +1,23 @@
 package org.mule.connectors.internal.operations;
 
+import java.io.InputStream;
 import java.lang.String;
 import org.mule.connectors.api.MongoPublishAttributes;
 import org.mule.connectors.internal.connection.MongoConnection;
+import org.mule.runtime.core.api.util.IOUtils;
 import org.mule.runtime.extension.api.annotation.param.Connection;
 import org.mule.runtime.extension.api.annotation.param.MediaType;
 import org.mule.runtime.extension.api.runtime.operation.Result;
 
-public class MongoOperations {
-  @MediaType("*/*")
-  public Result<String, MongoPublishAttributes> postMessage(@Connection MongoConnection connection,
-      String message, String destination) {
-    MongoPublishAttributes attributes = new MongoPublishAttributes(connection.getClientId(), destination);
-    return Result.<String,MongoPublishAttributes>builder()
-        .output(message)
-        .attributes(attributes)
-        .build();
-  }
+import org.bson.Document;
 
-  @MediaType("*/*")
-  public String helloWorld(@Connection MongoConnection connection, String name) {
-    return "Hello World!, " + name;
+public class MongoOperations {
+
+
+  public void insertDocument(@Connection MongoConnection connection, String database, String collection, InputStream document) {
+    connection.getMongoClient()
+            .getDatabase(database)
+            .getCollection(collection)
+            .insertOne(Document.parse(IOUtils.toString(document)));
   }
 }
